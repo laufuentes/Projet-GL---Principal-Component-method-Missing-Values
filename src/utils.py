059c96cbf17621b2e88e42a5c0c4a24809febf59
laufuentes <la.fuentes@gmail.com>
2024-03-11 19:33:24 +0100

    running

--- src/utils.py
+++ src/utils.py
@@ -2,7 +2,7 @@ import pandas as pd
 import numpy as np
 
 class FAMD:
-    def __init__(self, n_components=2, data=None, k1=None, k2=None):
+    def __init__(self, data, k1, k2, n_components=2):
         """Initialisation
 
         Args:
@@ -12,8 +12,8 @@ class FAMD:
         self.n_components = n_components
         self.k1 = k1
         self.k2 = k2
-        self.df_C0 = data[k1]
-        self.df_categ = data[k2]
+        self.df_C0 = data[self.k1]
+        self.df_categ = data[self.k2]
 
     def process_categ(self): 
         dummies = pd.get_dummies(self.df_categ)
@@ -23,28 +23,29 @@ class FAMD:
 
     def data_concat(self): 
         """Redefinition des données à chaque fois que l'on change df_C0 et df_categ"""
-        self.df = np.concatenate((self.df_C0, self.df_categ), axis=1)
+        self.df = pd.DataFrame(np.concatenate((self.df_C0, self.df_categ), axis=1), columns=np.concatenate((self.k1.tolist(), self.k2.tolist())))
         pass
 
     def ponderation(self): 
+        self.data_concat()
         self.df_C0 = self.df[self.k1] # redefini df_C0 avec le df actuel
         self.sj = self.df_C0.std(axis=0).to_numpy()
         new_df_C0 = self.df_C0 / self.sj
 
         self.df_categ = self.df[self.k2]
         self.sqrt_pj = np.sqrt(self.df_categ.sum(axis=0)/self.df_categ.shape[0]).to_numpy()
-        new_df_categ = self.df_categ / self.sqrtpj #mise a jour de df_C0
+        new_df_categ = self.df_categ / self.sqrt_pj #mise a jour de df_C0
 
         self.df_C0 = new_df_C0 # redefini df_categ avec le df actuel
         self.df_categ = new_df_categ / new_df_categ.sum(axis=0) ##TODO: verifier si la somme doit faire 1!!!
-        self.df = self.data_concat() # mise a jour de df_categ
+        self.data_concat() # mise a jour de df_categ
 
     def DM(self):
         """Function pour definir D et M à partir des valeur des données stockées en self"""
         # self.df() à laisser ? 
         # Calcul de D_sigma
         self.D = np.diag(np.concatenate((self.sj,self.sqrt_pj)))
-        D_moins_sqrt = np.sqrt(np.linalg.inv(np.diag(self.D)))
+        D_moins_sqrt = np.sqrt(np.linalg.inv(self.D))
         
         #Calcul de XD_sigma^(-0.5)
         self.XD_moins_sqrt = np.dot(self.df,D_moins_sqrt)
@@ -67,6 +68,6 @@ class FAMD:
         U, S, Vt = np.linalg.svd(self.XD_moins_sqrt - self.M)
 
         #Reconstruction dans une plus petite dimension
-        Z_p = pd.DataFrame(U[:,:self.n_components]@ np.diag(S)[:self.n_components,:self.n_components] @Vt[:self.n_components,:], columns=Ximp.columns, index=Ximp.index)
+        Z_p = pd.DataFrame(U[:,:self.n_components]@ np.diag(S)[:self.n_components,:self.n_components] @Vt[:self.n_components,:], columns=self.df.columns, index=self.df.index)
 
         return Z_p
